1935b626b24a8b72b59177d1426d0f018b3f4479
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-14 22:20:43 -0800

    Added:watch service to a directory

--- src/test/java/com/bridgelabz/employeepayrollservice/NIOFileAPITest.java
+++ src/test/java/com/bridgelabz/employeepayrollservice/NIOFileAPITest.java
@@ -51,4 +51,11 @@ public class NIOFileAPITest {
 		Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp"))
 				.forEach(System.out::println);
 	}
+	@Test
+	public void givenDirectory_WhenWatched_ShouldReturnAllActivities() throws IOException
+	{
+		Path dir = Paths.get(HOME+'/'+PLAY_WITH_NIO);
+		Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+		new Java8WatchServiceExample(dir).processEvents();
+	}
 }
