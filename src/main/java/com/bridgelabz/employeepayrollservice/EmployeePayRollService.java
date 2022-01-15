991ae782a23be58fe2923ff12c146ff8b331bee5
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-14 22:40:57 -0800

    Added:printing employee data to file

--- src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayRollService.java
+++ src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayRollService.java
@@ -18,33 +18,37 @@ public class EmployeePayrollService {
 	public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
 		this.employeePayrollList = employeePayrollList;
 	}
-	
+
 	private void readEmployeePayrollData(Scanner consoleInputReader) {
-		
+
 		System.out.println("Enter the Employee Id : ");
 		int id = consoleInputReader.nextInt();
 		System.out.println("Enter the Employee Name : ");
 		String name = consoleInputReader.next();
 		System.out.println("Enter the Employee Salary : ");
 		double salary = consoleInputReader.nextDouble();
-		
+
 		employeePayrollList.add(new EmployeePayrollData(id, name, salary));
 	}
-	
-	private void writeEmployeePayrollData() {
-		System.out.println("\nWriting Employee Payroll Roster to Console\n" + employeePayrollList);
+
+	public void writeEmployeePayrollData(IOService ioService) {
+		if (ioService.equals(IOService.CONSOLE_IO))
+			System.out.println("\nWriting Employee Payroll Roster to Console\n" + employeePayrollList);
+
+		else if (ioService.equals(IOService.FILE_IO))
+			new EmployeePayrollFileIOService().writeData(employeePayrollList);
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("Welcome To Employee Payroll Service Program\n");
-		ArrayList<EmployeePayrollData> employeePayrollList  = new ArrayList<EmployeePayrollData>();
+		ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<EmployeePayrollData>();
 		EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
 		Scanner consoleInputReader = new Scanner(System.in);
-		
+
 		employeePayrollService.readEmployeePayrollData(consoleInputReader);
-		employeePayrollService.writeEmployeePayrollData();
-		
+		employeePayrollService.writeEmployeePayrollData(IOService.CONSOLE_IO);
+
 	}
 
 }
---
+++ src/test/java/com/bridgelabz/employeepayrollservice/EmployeePayrollServiceTest.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.employeepayrollservice;
+
+import java.util.Arrays; 
+import org.junit.Test;
+import com.bridgelabz.employeepayrollservice.EmployeePayrollService.IOService;
+
+
+public class EmployeePayrollServiceTest 
+{
+	@Test
+	public void given3Employees_WhenWrittenToFile_ShouldMatchEmployeeEntries()
+	{
+		EmployeePayrollData[] arrayOfEmployees = {
+				new EmployeePayrollData(1, "Jeff Bezos", 100000.0),
+				new EmployeePayrollData(2, "Bill Gates", 200000.0),
+				new EmployeePayrollData(3, "Mark Zuckerberg", 300000.0)
+		};
+		EmployeePayrollService employeePayrollService;
+		employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmployees));
+		employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
+		
+	}
+}
