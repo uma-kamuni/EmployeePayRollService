446340503c9f7868feaff5782e7e9c86c234ad4a
uma-kamuni <kamuniumaramesh@gmail.com>
2022-01-14 21:31:53 -0800

    Welcome to employee pay roll service

---
+++ src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayRollService.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.employeepayrollservice;
+
+public class EmployeePayRollService {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Employee Pay Roll Service");
+	}
+
+}
